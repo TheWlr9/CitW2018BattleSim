2dba2c633cac6f876d48a3afc0d59039ec89c365
TheWlr9 <williamritchie@hotmail.ca>
2018-08-18 15:02:25 -0500

    Added beginning simulation method

--- src/Main.py
+++ src/Main.py
@@ -13,3 +13,11 @@ army2 = Army(army2Name)
 def createArmy():
     # Where you can create your own army type.
     pass
+
+def simulate():
+    while(army1.checkPopulation>0 and army2.checkPopulation>0):
+        pass # Put the simulation code here
+    if(army1.checkPopulation<=0):
+        print(army2Name+" wins!")
+    else:
+        print(army1Name+" wins!")
