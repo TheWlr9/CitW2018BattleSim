8e0a634e4043d2f41e2e120f24def61d13279859
TheWlr9 <williamritchie@hotmail.ca>
2018-08-17 20:07:01 -0500

    Added some file code, and file opening

--- Main.c
+++ Main.c
@@ -6,16 +6,32 @@
 #include <string.h>
 
 int main(){
-    char battle1[100];
-    char battle2[100];
+    char army1Name[100];
+    char army2Name[100];
+    FILE* army1,* army2;
 
     printf("Select a battle... ");
-    scanf("%s",battle1);
+    scanf("%s",army1Name);
 
     printf("Select the second battle... ");
-    scanf("%s",battle2);
+    scanf("%s",army2Name);
 
-    printf("Army 1: %s\tArmy 2: %s",battle1,battle2);
+    printf("Army 1: %s\tArmy 2: %s",army1Name,army2Name);
+
+    army1= fopen(army1Name,"r");
+    army2= fopen(army2Name,"r");
+    if(army1 && army2){
+
+    }
+    else{
+        //There was an error with finding the army data. (maybe it doesn't exist?)
+        if(!army1)
+            printf("%s is an invalid army name",army1Name);
+        if(!army2)
+            printf("%s is an invalid army name",army2Name);
+
+        return 1; //Exit with an error code
+    }
 
     return 0;
 }
